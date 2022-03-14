1ba560ca62a6fe0d34a7a6123e22913a37949534
merior nordway <meriornordway@yandex.ru>
2022-03-14 16:40:12 +0300

    созданы переменные и остальные приколюхи

--- script.js
+++ script.js
@@ -1,2 +1,17 @@
-let snake_case = 10;
-const width = 11;
+const title = document.querySelector('.title');
+const screens = document.querySelector('.screens');
+const screenPrice = document.querySelector('.screenPrice');
+const rollback = document.querySelector('.rollback');
+const fullPrice = document.querySelector('.fullPrice');
+const adaptive = document.querySelector('.adaptive');
+
+console.log(title)
+console.log(screens)
+console.log(screenPrice)
+console.log(rollback)
+console.log(fullPrice)
+console.log(adaptive)
+
+console.log('все работает')
+
+alert('пока не работает надо закрыть')
