778595beb636dfdf1509ff55db1301c435757379
merior nordway <meriornordway@yandex.ru>
2022-03-19 13:20:49 +0300

    урок три надеюс правильно но я запуталась в логах

--- script.js
+++ script.js
@@ -1,24 +1,59 @@
-let title = 'строка с названием проекта';
-let screens = 'Простые, Сложные, Интерактивные';
-let screenPrice = 1234
+'use strict';
+
+let title = prompt('Как называется ваш проект?');
+console.log(title);
+
+let screens = prompt('Какие типы экранов нужно разработать?(напишите через запятую) ');
+let newScreens = screens.split(", ");
+
+let screenPrice = +prompt('Сколько будет стоить данная работа?');
 let rollback = Math.round(Math.random()*100);
-let fullPrice = Math.round(Math.random()*10000);
-let adaptive = Boolean(Math.round(Math.random()));
 
-let newScreens = screens.toLowerCase();
-let newNewScreens = newScreens.split(", ");
+// let fullPrice = Math.round(Math.random()*10000);
+let adaptive = confirm('Нужен ли адаптив на сайте?');
+
+let service1 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice1 = +prompt('Сколько будет стоить данная работа?');
+
+let service2 = prompt('Какой дополнительный тип услуги нужен?');
+let servicePrice2 = +prompt('Сколько будет стоить данная работа?');
 
 
-console.log(title, fullPrice, adaptive);
-console.log(screens.length);
-console.log('Стоимость верстки экранов ' + screenPrice + '$ ' + 'Стоимость разработки сайта ' + fullPrice + '$');
 
-console.log(newNewScreens);
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let rollbackPrice = fullPrice / 100 * rollback
+let servicePercentPrice = fullPrice - rollbackPrice
 
-console.log(fullPrice * (rollback/100));
+// console.log(title, fullPrice, adaptive);
+// console.log(screens.length);
+// console.log('Стоимость верстки экранов ' + screenPrice + '$ ' + 'Стоимость разработки сайта ' + fullPrice + '$');
+
+// console.log(newScreens);
+
+// console.log(fullPrice * (rollback/100));
 
 
 
 // console.log('все работает но неопределено')
 
-alert('пока не работает надо закрыть')
+if (fullPrice >= 30000) {
+    alert('Даем скидку в 10%')
+} else if (fullPrice >= 15000) {
+    alert('Даем скидку в 5%')
+} else if (0 < fullPrice) {
+    alert('Скидка не предусмотрена')
+} else if (fullPrice <= 0) {
+    alert('Что то пошло не так')
+}
+
+console.log(newScreens)
+console.log(rollbackPrice)
+console.log(servicePercentPrice)
+
+console.log(screenPrice);
+console.log(servicePrice1);
+console.log(servicePrice2);
+console.log(fullPrice);;
+
+console.log("Стоимость верстки экранов", screenPrice, "рублей и",
+ "Стоимость разработки сайта", fullPrice, "рублей")
