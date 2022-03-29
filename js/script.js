0f23913b8944f2973f99390e94341e381552d36a
merior nordway <meriornordway@yandex.ru>
2022-03-29 15:12:12 +0300

    lesson 08  vrode easy

---
+++ js/script.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const title = document.getElementsByTagName('h1')
+const button = document.getElementsByClassName('handler_btn')
+const cross = document.querySelector('.screen-btn')
+const otherPercentItems = document.querySelectorAll('.other-items.percent')
+const otherNumberItems = document.querySelectorAll('.other-items.number')
+const range = document.querySelector('.rollback input')
+const span = document.querySelector('.rollback span')
+const totalInput1 = document.getElementsByClassName('total-input')
+const totalInput2 = document.getElementsByClassName('total-input')
+const totalInput3 = document.getElementsByClassName('total-input')
+const totalInput4 = document.getElementsByClassName('total-input')
+const totalInput5 = document.getElementsByClassName('total-input')
+let screen = document.querySelectorAll('.screen')
+
+
+console.log(title[0]);
+console.log(button[0]);
+console.log(cross)
+console.log(otherPercentItems)
+console.log(otherNumberItems)
+console.log(range)
+console.log(span)
+console.log(totalInput1[0])
+console.log(totalInput2[1])
+console.log(totalInput3[2])
+console.log(totalInput4[3])
+console.log(totalInput5[4])
+console.log(screen)
