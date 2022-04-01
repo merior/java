471c64ba4f31c19b7e4e2b76ab7217fd6af517e3
merior nordway <meriornordway@yandex.ru>
2022-04-01 13:16:07 +0300

    10 урок книги

--- js/script.js
+++ js/script.js
@@ -1,30 +1,41 @@
-'use strict'
-
-const title = document.getElementsByTagName('h1')
-const button = document.getElementsByClassName('handler_btn')
-const cross = document.querySelector('.screen-btn')
-const otherPercentItems = document.querySelectorAll('.other-items.percent')
-const otherNumberItems = document.querySelectorAll('.other-items.number')
-const range = document.querySelector('.rollback input')
-const span = document.querySelector('.rollback span')
-const totalInput1 = document.getElementsByClassName('total-input')
-const totalInput2 = document.getElementsByClassName('total-input')
-const totalInput3 = document.getElementsByClassName('total-input')
-const totalInput4 = document.getElementsByClassName('total-input')
-const totalInput5 = document.getElementsByClassName('total-input')
-let screen = document.querySelectorAll('.screen')
+const books = document.querySelectorAll('.books')
+const book = document.querySelectorAll('.book')
+const add = document.querySelector('.adv')
+const title3 = document.querySelectorAll('a')
+const lists = document.querySelectorAll('ul')
+const elem = document.querySelectorAll('li')
 
 
-console.log(title[0]);
-console.log(button[0]);
-console.log(cross)
-console.log(otherPercentItems)
-console.log(otherNumberItems)
-console.log(range)
-console.log(span)
-console.log(totalInput1[0])
-console.log(totalInput2[1])
-console.log(totalInput3[2])
-console.log(totalInput4[3])
-console.log(totalInput5[4])
-console.log(screen)
+document.body.style.backgroundImage = "url('../image/you-dont-know-js.jpg')";
+
+
+add.remove()
+
+book[0].before(book[1])
+book[5].after(book[2])
+book[3].before(book[4])
+
+elem[3].after(elem[6])
+elem[9].after(elem[2])
+elem[6].after(elem[8])
+
+elem[47].after(elem[55])
+elem[55].after(elem[49])
+elem[49].after(elem[50])
+elem[53].after(elem[51])
+
+
+title3[4].textContent = 'Книга 3. this и Прототипы Объектов'
+
+const createNewLi = document.createElement('li');
+createNewLi.textContent = 'Глава 8: За пределами ES6'
+
+book[2].querySelector('ul').append(createNewLi);
+createNewLi.after(elem[26])
+
+console.log(books)
+console.log(book)
+console.log(title3)
+
+console.log(lists)
+console.log(elem)
